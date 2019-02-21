3d4426c9db4592c6b228296ee6d7e7bd2b8dc622
Lorenz Kuhn <kuhnl@student.ethz.ch>
2019-02-21 09:03:54 +0000

    first running version on DO

--- src/boot.sh
+++ src/boot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 source activate cycleGAN
-exec gunicorn -b 0.0.0.0:5000 --access-logfile - --error-logfile - application_server:app
+exec gunicorn -b 0.0.0.0:2375 --access-logfile - --error-logfile - application_server:app
 
 
