917d3e9f1f2472168e4726609206e62831fa6bc3
Lorenz <lorenz.kuhn@bluewin.ch>
2019-02-21 10:39:57 +0200

    actually fix dockerfile

--- src/boot.sh
+++ src/boot.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 source activate cycleGAN
-exec gunicorn -b :5000 --access-logfile - --error-logfile - application_server:app
+exec gunicorn -b 0.0.0.0:5000 --access-logfile - --error-logfile - application_server:app
+
+
