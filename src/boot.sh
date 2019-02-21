9835423704f16f00346aab34c11a54dc40c3058c
Lorenz Kuhn <kuhnl@student.ethz.ch>
2019-02-21 09:27:56 +0000

    use port 80 instead of 2375

--- src/boot.sh
+++ src/boot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 source activate cycleGAN
-exec gunicorn -b 0.0.0.0:2375 --access-logfile - --error-logfile - application_server:app
+exec gunicorn --workers=2 -b 0.0.0.0:2375 --access-logfile - --error-logfile - application_server:app
 
 
