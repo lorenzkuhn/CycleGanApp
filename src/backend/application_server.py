0a52c8aad9864e0c268712d3eba5ef697088aa0b
phil9987 <philipjunker@gmail.com>
2019-02-24 00:30:16 +0200

    uncommented model load

--- src/backend/application_server.py
+++ src/backend/application_server.py
@@ -120,5 +120,5 @@ if __name__ != '__main__':
     gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
-    #load_model('gpu_model')
+    load_model('gpu_model')
     load_transform_function()
