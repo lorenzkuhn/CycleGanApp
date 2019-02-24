82b22c3d527137f233e30e4f9db73e525222b7fe
phil9987 <philipjunker@gmail.com>
2019-02-24 17:13:43 +0200

    nginx

--- src/boot.sh
+++ src/boot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Use the following to pipe log to stdout in debug mode
-exec gunicorn --workers=3 --timeout 120 -b 0.0.0.0:8080 --access-logfile - --error-logfile - --log-level=debug application_server:app
+exec gunicorn --workers=5 --timeout 120 -b 0.0.0.0:8080 --access-logfile - --error-logfile - --log-level=debug application_server:app
 
 # Use this to write log to access.log and error.log (redirects all std output to error.log)
 #exec gunicorn --workers=5 --timeout 120 -b 0.0.0.0:8080 --access-logfile ./access.log --error-logfile ./error.log --log-level=debug --capture-output application_server:app
