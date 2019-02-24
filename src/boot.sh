d3d2bd531c9cedabe174da46fbfba0abecc0c13a
phil9987 <philipjunker@gmail.com>
2019-02-24 16:40:10 +0200

    debug nginx

--- src/boot.sh
+++ src/boot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+exec nginx -c /home/cyclegan/nginx.conf 
 # Use the following to pipe log to stdout in debug mode
-# exec gunicorn --workers=3 --timeout 120 -b 0.0.0.0:2375 --access-logfile - --error-logfile - --log-level=debug application_server:app
+exec gunicorn --workers=3 --timeout 120 -b 0.0.0.0:8080 --access-logfile - --error-logfile - --log-level=debug application_server:app
 
 # Use this to write log to access.log and error.log (redirects all std output to error.log)
-exec nginx -c /home/cyclegan/nginx.conf 
-exec gunicorn --workers=5 --timeout 120 -b 0.0.0.0:8080 --access-logfile ./access.log --error-logfile ./error.log --log-level=debug --capture-output application_server:app
+#exec gunicorn --workers=5 --timeout 120 -b 0.0.0.0:8080 --access-logfile ./access.log --error-logfile ./error.log --log-level=debug --capture-output application_server:app
