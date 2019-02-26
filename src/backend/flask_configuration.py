77624fb5ce498ba0bcb98caf00533e64a128a278
kkleindev <kleinkevin.lu@gmail.com>
2019-02-26 23:22:05 +0100

    Reimplement behavior of servside failures.

--- src/backend/flask_configuration.py
+++ src/backend/flask_configuration.py
@@ -3,7 +3,5 @@ Path(UPLOAD_FOLDER).mkdir(exist_ok=True)
 RESPONSE_FOLDER = Path.cwd() / 'response/'
 Path(RESPONSE_FOLDER).mkdir(exist_ok=True)
 
-# Set limit on file size of uploaded files. 50 * 1024 * 1024 is 50 MB.
-MAX_CONTENT_LENGTH = 50 * 1024 * 1024
 UPLOAD_FOLDER = UPLOAD_FOLDER
 RESPONSE_FOLDER = RESPONSE_FOLDER
