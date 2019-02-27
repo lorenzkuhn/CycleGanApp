cc22dbffd6b4f54ce6419ae59ec204577768e08c
phil9987 <philipjunker@gmail.com>
2019-02-27 12:12:06 +0200

    gunicorn fix

--- src/boot.sh
+++ src/boot.sh
@@ -3,4 +3,5 @@
 #exec gunicorn --workers=5 --timeout 120 -b 0.0.0.0:8080 --access-logfile - --error-logfile - --log-level=debug application_server:app
 
 # Use this to write log to access.log and error.log (redirects all std output to error.log)
-exec gunicorn --workers=5 --timeout 120 -b 0.0.0.0:8080 --access-logfile /persistentlogs/gunicorn_access.log --error-logfile /persistentlogs/gunicorn_error.log --log-level=info --capture-output application_server:app
+#exec gunicorn --workers=5 --timeout 120 -b 0.0.0.0:8080 --access-logfile /persistentlogs/gunicorn_access.log --error-logfile /persistentlogs/gunicorn_error.log --log-level=info --capture-output application_server:app
+exec gunicorn --workers=5 --timeout 120 -b 0.0.0.0:8080 --access-logfile gunicorn_access.log --error-logfile gunicorn_error.log --log-level=info --capture-output application_server:app
